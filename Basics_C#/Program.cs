6bfea092365c213f2cf3d668974b32d29a6f1efc
Monica Iliescu <monica.iliescu@amdaris.com>
2024-03-13 12:17:31 +0200

    is and as operators

--- Basics_C#/Program.cs
+++ Basics_C#/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-
+using System.ComponentModel.Design;
 using Basics_C_.Class;
 using Basics_C_.Interface;
 
@@ -21,20 +21,43 @@ namespace Basics
             Console.WriteLine(b.calculateArea());
             Console.WriteLine(c.calculateArea(2,6));
 
-            
-            Square square = new Square(8);
-            Triangle triangle = new Triangle(5, 5, 7);
 
-           
-            IShape[] shapes = { square, triangle,a,b,c };
 
             //using IEnumeration<IShape>
-            foreach (IShape shape in shapes)
+
+            /*
+              Square square = new Square(8);
+              Triangle triangle = new Triangle(5, 5, 7);
+              IShape[] shapes = { square, triangle, a, b, c };
+
+              foreach (IShape shape in shapes)
+              {
+                 shape.printDetails();
+              }
+            */
+
+
+            // is and as operators
+
+            Shape[] shapes = { new Square(2), new Triangle(5, 5, 5), new Triangle(3, 4, 4) };
+
+            foreach (var shape in shapes)
             {
-               shape.printDetails();
+                if (shape is Square)
+                {
+                    Square square = shape as Square;
+                    Console.WriteLine($"Square area: {square.calculateArea()}");
+                    square.printDetails();
+                }
+                else if (shape is Triangle)
+                {
+                    Triangle triangle = shape as Triangle;
+                    Console.WriteLine($"Triangle area: {triangle.calculateArea()}");
+                    triangle.printDetails();
+                }
             }
 
-           
+
         }
     }
 }
